ee53d4f57616fd168a8c5b062d7d291568300e86
hjoo830 <hjoo830@naver.com>
2024-08-30 00:04:17 +0900

    ✨ Card 컴포넌트 만들기

---
+++ create-react-app/my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="Hyoju Hwang" backgroundColor="#5faae3">
+      <p>안녕하세요 효주입니다.</p>
+      <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
